1a11f6578c7e4faa8d15bed8427be8f9c8b08348
karim076 <59794989+karim076@users.noreply.github.com>
2022-07-10 23:03:53 +0200

    Added card and card big & grid containter

--- index.php
+++ index.php
@@ -11,9 +11,20 @@
             
         </header>
         <main>
-            <div class="grid">
-                
-            </div>
+        <div class="grid">
+            <div class="card">ONE</div>
+            <div class="card-big">TWO</div>
+            <div class="card">THREE</div>
+            <div class="card">FOUR</div>
+            <div class="card">FIVE</div>
+            <div class="card">SIX</div>
+            <div class="card">SEVEN</div>
+            <div class="card">EIGHT</div>
+            <div class="card-big">NINE</div>
+            <div class="card">TEN</div>
+            <div class="card">ELEVEN</div>
+            <div class="card">TWELVE</div>
+        </div>
         </main>
         <footer>
 
