c0a1573823ffe0eaf770628a2f2c0e9f749b32c0
karim076 <59794989+karim076@users.noreply.github.com>
2022-07-10 22:57:17 +0200

    nl

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <<!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
     <head>
         <title></title>
         <meta charset="UTF-8">
@@ -14,7 +14,7 @@
 
         </main>
         <footer>
-            
+
         </footer>
     </body>
 </html>
